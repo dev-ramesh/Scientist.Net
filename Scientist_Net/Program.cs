865cbb02b70a3f2c8735ae2031d20a7faab8d80e
ramesh <rameshp89@yahoo.co.in>
2016-12-14 19:52:51 +0000

    publishing to sql server , performance details.

--- Scientist_Net/Program.cs
+++ Scientist_Net/Program.cs
@@ -14,18 +14,22 @@ namespace Scientist_Net
             var smptSender = new SmtpMailSender();
             var cloudSender = new CloudMailSender();
 
-            Scientist.ResultPublisher = new  ConsolePublisher();
+            Scientist.ResultPublisher = new  SqlServerPublisher();
 
             string[] emailAddresses = new string[] { "wonderful@gmail.com","pissoff.net","killme@com","whoisthat.com"};
 
             foreach (var emailAddress in emailAddresses)
             {
+                
 
-                bool isValidEmail = Scientist.Science<bool>("cloud-email-gateway", experiment=> {
+                    bool isValidEmail = Scientist.Science<bool>("cloud-email-gateway", experiment=> {
+
+
+                    experiment.AddContext("email address",emailAddress);
 
                     experiment.Use(()=>smptSender.isValidEmail(emailAddress));
 
-                experiment.Try(() => cloudSender.isValidEmail(emailAddress));
+                experiment.Try("Cloud sender",() => cloudSender.isValidEmail(emailAddress));
 
 
                 });
---
+++ Scientist_Net/SqlServerPublisher.cs
@@ -0,0 +1,55 @@
+﻿using GitHub;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scientist_Net
+{
+    class SqlServerPublisher:IResultPublisher
+    {
+
+        public Task Publish<T, TClean>(Result<T, TClean> result)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ExperimentDb"].ConnectionString;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                string sql = "INSERT into SqlPublisher (Date, AppVersion, Name, IsMatch, ControlValue, CandidateValue, ControlDurationMs, CandidateDurationMs, Context) VALUES (@Date, @AppVersion, @Name, @IsMatch, @ControlValue, @CandidateValue, @ControlDurationMs, @CandidateDurationMs, @Context)";
+
+                connection.Open();
+
+                using (SqlCommand insertCommand = new SqlCommand(sql, connection))
+                {
+                    insertCommand.Parameters.AddWithValue("@Date", DateTime.Now);
+                    insertCommand.Parameters.AddWithValue("@AppVersion", Assembly.GetExecutingAssembly().GetName().Version.ToString());
+                    insertCommand.Parameters.AddWithValue("@Name", result.ExperimentName);
+                    insertCommand.Parameters.AddWithValue("@IsMatch", result.Matched);
+                    insertCommand.Parameters.AddWithValue("@ControlValue", result.Control.Value);
+                    insertCommand.Parameters.AddWithValue("@ControlDurationMs", result.Control.Duration.TotalMilliseconds);
+
+                    if (result.Candidates.Any())
+                    {
+                        insertCommand.Parameters.AddWithValue("@CandidateValue", result.Candidates[0].Value);
+                        insertCommand.Parameters.AddWithValue("@CandidateDurationMs", result.Candidates[0].Duration.TotalMilliseconds);
+                    }
+
+                    if (result.Contexts.Any())
+                    {
+                        insertCommand.Parameters.AddWithValue("@Context", result.Contexts.First().Value);
+                    }
+
+                    insertCommand.ExecuteNonQuery();
+                }
+            }
+
+            return Task.FromResult(0);
+        }
+
+       
+    }
+}
